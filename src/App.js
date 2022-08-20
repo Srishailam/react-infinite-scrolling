661ba0696446958619714cbb65c4166e3256604e
Srishailam Dasari <sriyd531@gmail.com>
2022-08-20 00:13:25 -0400

    scroll changes

--- src/App.js
+++ src/App.js
@@ -9,20 +9,34 @@ function App() {
   const [isLoading, setIsLoading] = React.useState(false);
   const [paginate, setPaginate] = React.useState(15);
 
-React.useEffect(() => {
+const fetchMerchantData = () => {
+  setIsLoading(true);
+  fetch('https://staging.api.1m.app/api/users/nearby/merchants')
+  .then(data => data.json())
+  .then(data => {
+    setMerchantData(data);
+    setIsLoading(false);
+  })
+  .catch();
+}
 
-  const fetchMerchantData = () => {
-    setIsLoading(true);
-    fetch('https://staging.api.1m.app/api/users/nearby/merchants')
-    .then(data => data.json())
-    .then(data => {
-      setMerchantData(data);
-      setIsLoading(false);
-    })
-    .catch();
+const handleScroll = (e) => {
+  let userScrollHeight = e.target.documentElement.scrollTop;
+  let windowHeight = window.innerHeight;
+  let scrollHeight = e.target.documentElement.scrollHeight;
+
+  if (windowHeight + userScrollHeight +1 >= scrollHeight) {
+    setPaginate(paginate => paginate + 15);
   }
+};
 
+React.useEffect(() => {
   fetchMerchantData();
+  window.addEventListener('scroll', handleScroll);
+
+  return () => {
+    window.removeEventListener('scroll', handleScroll);
+  }
 }, [])
 
 
